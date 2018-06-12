ef4ef721ab8934013b5790f40a9a7efe4af452aa
diwakark89 <diwakark89@users.noreply.github.com>
2018-06-12 08:44:22 +0530

    Initial setup

---
+++ Algo/src/com/org/excercise/Javatest.java
@@ -0,0 +1,31 @@
+package com.org.excercise;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Javatest {
+	public static void main(String []args) {
+		ConcurrentHashMap<String, Stock> dataMap=new ConcurrentHashMap<String, Stock>();
+		String key="1";
+		double val=10;
+		Stock stock=null;
+		if(dataMap.containsKey(key)==true) {
+			stock=dataMap.get(key);
+			double total=stock.avg*stock.count;
+			stock.count++;
+			stock.avg=(total+val)/stock.count;
+		}
+		else {
+			stock=new Stock(1, val);
+		}
+		dataMap.put(key, stock);
+	}
+	
+}
+class Stock{
+	int count;
+	double avg;
+	public Stock(int count,double avg) {
+		this.count=count;
+		this.avg=avg;
+	}
+}
